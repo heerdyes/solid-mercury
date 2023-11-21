699551ebe8d545eba86d5cc4ac4595205fb14d49
Heerdyes Mahapatro <heerdyes.mahapatro@gmail.com>
2023-11-21 12:42:58 +0530

    example api for customer create

--- src/middleware/mercuryvapor/shopctl/views.py
+++ src/middleware/mercuryvapor/shopctl/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.core.exceptions import PermissionDenied
+import json
+from .models import *
 
 # Create your views here.
+# crud for customer, source and category
+def create_customer(rq):
+    if rq.method != 'POST':
+        raise PermissionDenied
+    jsonstr = rq.body.decode('utf-8')
+    cust = json.loads(jsonstr)
+    print(cust)
+    # create customer
+    c = Customer(name=cust['name'], phnum=cust['phnum'])
+    c.save()
+    return JsonResponse({
+        'status': 'OK'
+    })
